76e85e114be94dd9a7547ab2b4e1eba95174304c
Ryan Appel <appel@fvtc.edu>
2024-03-28 23:08:37 -0500

    completed starter code for assignment

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.static('public'))
+
+const todos = [
+	{ id: 1, item: 'Learn JavaScript', complete: false },
+	{ id: 2, item: 'Learn Express', complete: false },
+	{ id: 3, item: 'Build a To Do App', complete: false }
+]
+
+app.get('/', (request, response) => {
+	response.sendFile('index.html', { root })
+})
+
+
+
+// GET /api/todos
+
+// POST /api/todos
+
+// PUT /api/todos/:id
+
+
+
+const message = `Server running: http://localhost:${port}`
+app.listen(port, () => console.log(message))
