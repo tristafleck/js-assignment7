d1e9abe2638b82e498fcb92b709359853adbe8ad
Ryan Appel <appel@fvtc.edu>
2024-03-28 23:21:59 -0500

    changed port to 3000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.static('public'))
