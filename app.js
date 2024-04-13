6260f7d6a4c9fff4bb5e8bdb396eed2cc10cd82b
tristafleck <156954824+tristafleck@users.noreply.github.com>
2024-04-13 06:12:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,29 @@ app.get('/', (_, response) => {
 	response.sendFile('index.html', { root })
 })
 
-
+app.get('/api/todos', (_,response) => {
+	response.json(todos);
+});
 
 // GET /api/todos
 
 // POST /api/todos
 
+app.post('/api/todos', (request, response) => {
+	const {item} = request.body;
+	const id = todos.length + 1;
+	const complete = false;
+	todos.push({id, item, complete });
+	response.json({id});
+});
+
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (request, response) => {
+	const {id} = request.parmas;
+	const task = todos.find(todo => todo.id.toString() == id)
+	task.complete = !task.complete;
+	response.json(task);
+});
 
 
 
